07ae083c546f24423f860a9e864f5f739fef65b9
mustafaizzet1 <ymustafaizzet@gmail.com>
2023-12-22 22:54:13 +0300

    update post  and change password

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../helper/AuthContext";
 
 function Profile() {
   let { id } = useParams();
   let navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [listOfPosts, setListOfPosts] = useState([]);
+  const { authState } = useContext(AuthContext);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/auth/basicinfo/${id}`).then((response) => {
@@ -25,6 +27,10 @@ function Profile() {
       <div className="basicInfo">
         {" "}
         <h1> Username: {username} </h1>
+        {authState.username==username && (
+          <button onClick={()=> navigate('/changepassword') }>Change My Password</button>
+        )}
+        
       </div>
       <div className="listOfPosts">
         {listOfPosts.map((value, key) => {
